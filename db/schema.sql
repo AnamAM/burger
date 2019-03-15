09c26b2cd370fb573734c606d55276f8a33e3674
Anam Mohammad <m.anam0603@gmail.com>
2019-03-14 22:40:29 -0500

    mostly working

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+id INTEGER(30) AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(100) NOT NULL,
+devoured BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Classic Cheeseburger", false),
+("Jalapeno Popper Burger", false),
+("Hopdoddy Burger", false),
+("Five Guys Burger", false),
+("Screamin' Nachoes Burger", false);
+
+SELECT * FROM burgers;
